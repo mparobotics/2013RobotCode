887d297243bf63451e2da18f116992ba9a0d87f5
Team Thirty-Nine Twenty-Six <aegrabanski@yahoo.com>
2013-01-18 08:55:53 -0600

    Created CAN code [1 Jag] controlled by joystick

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -1,46 +1,37 @@
 #include "WPILib.h"
 
-/**
- * This is a demo program showing the use of the RobotBase class.
- * The SimpleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- */ 
 class RobotDemo : public SimpleRobot
 {
 	RobotDrive myRobot; // robot drive system
 	Joystick stick; // only joystick
+	CANJaguar jag1; //Declartion for Jaguar 1
 
 public:
 	RobotDemo(void):
 		myRobot(1, 2),	// these must be initialized in the same order
-		stick(1)		// as they are declared above.
+		stick(1),		// as they are declared above.
+		jag1(5,CANJaguar::kVoltage)
 	{
 		myRobot.SetExpiration(0.1);
 	}
 
-	/**
-	 * Drive left & right motors for 2 seconds then stop
-	 */
 	void Autonomous(void)
 	{
-		myRobot.SetSafetyEnabled(false);
-		//myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
-		Wait(2.0); 				//    for 2 seconds
-		//myRobot.Drive(0.0, 0.0); 	// stop robot
+		
 	}
 
-	/**
-	 * Runs the motors with arcade steering. 
-	 */
+	
 	void OperatorControl(void)
 	{
 		myRobot.SetSafetyEnabled(true);
+		jag1.SetSpeedReference(CANJaguar::kSpeedRef_None);
+		jag1.EnableControl();
 		while (IsOperatorControl())
 		{
-			//myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
-			printf("%s", "hello");
-			Wait(0.005);				// wait for a motor update time
+			jag1.Set(stick.GetY()*-11.0*1.09); //Set Jag
+			printf("%f", jag1.GetOutputVoltage());
+			printf("\n");
+			Wait(0.01);				// wait for a motor update time
 		}
 	}
 	
