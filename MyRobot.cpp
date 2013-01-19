cda94142674b5e28fa41c4ccd035bcd6d1754b97
TEAM3926ONE <agrabanski13@moundsparkacademy.org>
2013-01-19 15:38:55 -0600

    Added control for 8 solenoids (controlled by buttons)

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -7,13 +7,27 @@ class RobotDemo : public SimpleRobot
 	Joystick stick; // only joystick
 	CANJaguar jag1; //Declartion for Jaguar 1
 	Solenoid sol1;
+	Solenoid sol2;
+	Solenoid sol3;
+	Solenoid sol4;
+	Solenoid sol5;
+	Solenoid sol6;
+	Solenoid sol7;
+	Solenoid sol8;
 
 public:
 	RobotDemo(void):
 		myRobot(1, 2),	// these must be initialized in the same order
 		stick(1),		// as they are declared above.
 		jag1(5,CANJaguar::kVoltage),
-		sol1(1)
+		sol1(1),
+		sol2(2),
+		sol3(3),
+		sol4(4),
+		sol5(5),
+		sol6(6),
+		sol7(7),
+		sol8(8)
 	{
 		myRobot.SetExpiration(0.1);
 	}
@@ -34,8 +48,15 @@ public:
 			jag1.Set(stick.GetY()*-11.0*1.09); //Set Jag
 			printf("%f", jag1.GetOutputVoltage());
 			printf("\n");
-			Wait(0.01);				// wait for a motor update time
-			sol1.Set(true);
+			Wait(.05);				// wait for a motor update time
+			sol1.Set(stick.GetRawButton(1));
+			sol2.Set(stick.GetRawButton(2));
+			sol3.Set(stick.GetRawButton(3));
+			sol4.Set(stick.GetRawButton(4));
+			sol5.Set(stick.GetRawButton(5));
+			sol6.Set(stick.GetRawButton(6));
+			sol7.Set(stick.GetRawButton(7));
+			sol8.Set(stick.GetRawButton(8));
 		}
 	}
 	
