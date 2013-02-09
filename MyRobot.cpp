85b6b4bedf4333a53e3ec010991383a268bd176e
TEAM3926ONE <agrabanski13@moundsparkacademy.org>
2013-02-09 15:06:10 -0600

    Added stupid matrix library, cleanup
    [inversion routine pulled from MESA]

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -1,21 +1,13 @@
 #include "WPILib.h"
 #include <ADXL345_SPI.h>
 #include <Solenoid.h>
+#include "matrix.h"
 
 class RobotDemo : public SimpleRobot
 {
 	RobotDrive myRobot; // robot drive system
 	Joystick stick; // only joystick
 	CANJaguar jag1; //Declartion for Jaguar 1
-	Solenoid sol1;
-	Solenoid sol2;
-	Solenoid sol3;
-	Solenoid sol4;
-	Solenoid sol5;
-	Solenoid sol6;
-	Solenoid sol7;
-	Solenoid sol8;
-	Relay compressor;
 	Gyro gyro;
 
 public:
@@ -23,15 +15,6 @@ public:
 		myRobot(1, 2),	// these must be initialized in the same order
 		stick(1),		// as they are declared above.
 		jag1(5,CANJaguar::kVoltage),
-		sol1(1),
-		sol2(2),
-		sol3(3),
-		sol4(4),
-		sol5(5),
-		sol6(6),
-		sol7(7),
-		sol8(8),
-		compressor(1,Relay::kForwardOnly),
 		gyro(1)
 	{
 		myRobot.SetExpiration(0.1);
@@ -51,23 +34,10 @@ public:
 		gyro.Reset(); 
 		while (IsOperatorControl())
 		{
-			jag1.Set(stick.GetY()*-11.0*1.09); //Set Jag
+			jag1.Set(stick.GetY()*-12.0); //Set Jag
 			//printf("%f", jag1.GetOutputVoltage());
 			//printf("\n");
 			Wait(.05);				// wait for a motor update time
-			sol1.Set(stick.GetRawButton(1));
-			sol2.Set(stick.GetRawButton(2));
-			sol3.Set(stick.GetRawButton(3));
-			sol4.Set(stick.GetRawButton(4));
-			sol5.Set(stick.GetRawButton(5));
-			sol6.Set(stick.GetRawButton(6));
-			sol7.Set(stick.GetRawButton(7));
-			//sol8.Set(stick.GetRawButton(8));
-			if (stick.GetRawButton(8)) {
-				compressor.Set(Relay::kOn);
-			} else {
-				compressor.Set(Relay::kOff);
-			}
 			printf("%f", gyro.GetAngle());
 		}
 	}
