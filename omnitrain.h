b05c8d9afe8a4787709889a279206d912c6bde9b
TEAM3926ONE <agrabanski13@moundsparkacademy.org>
2013-02-10 13:26:37 -0600

    Finishing up the branch -- working drivetrain code :D

--- omnitrain.h
+++ omnitrain.h
@@ -17,44 +17,48 @@ double drivetrain_inverse_mat[16];
  */
 bool setupOmniTrain(double k1, double k2) {
 	//[see drawings, otherwise, voodoo]
-	double J = (1 - p) / distanceFormula(spacing_length * .5, p * spacing_width);
-	double T = p / distanceFormula(spacing_length * .5, (1 - p) * spacing_width);
+	double J = (1.0 - p) / distanceFormula(spacing_length * .5, p * spacing_width);
+	double T = p / distanceFormula(spacing_length * .5, (1.0 - p) * spacing_width);
 	
-	double a = k1 * (1 - p);
+	double a = k1 * (1.0 - p);
 	double b = k1 * p;
 	double c = k2 * J;
 	double d = k2 * T;
 	
-	double drivetrain_input_mat[16] = {a,-a,b,-b,a,a,b,b,-c,c,d,-d,1,1,1,1};
+	double drivetrain_input_mat[16] = {a,-a,b,-b,a,a,b,b,-c,c,d,-d,1.0,1.0,-1.0,1.0};
 	
-	bool errorcode = !InvertMatrix(drivetrain_input_mat, drivetrain_inverse_mat);
-	return error;
+	bool errorcode = !invertMatrix(drivetrain_input_mat, drivetrain_inverse_mat);
+	return errorcode;
 }
 
 /*this function should have low values [close to 0.0, relative to 1.0]
 in the domain of [-1.0, 1.0], and prohibitively high values outside that range.
 In addition, the prohibitively high values should increase as x gets further from
 zero.*/ 
-double herusuticFunc(double x) {
+double heuristicFunc(double x) {
 	if (x > 1.0) {
-		return 250.0 * x - 250.0;
+		return 50.0 * x - 50.0;
 	}
 	if (x < -1.0) {
-		return -250.0 * x - 250.0;
+		return -50.0 * x - 50.0;
 	}
 	return 0.0;
 }
 
-/* input: array of drivetrain voltage scalars. Output: rough power consumption */
-double powerConsumption(const double drive[4]) {
-	return fabs(drive[0]) + fabs(drive[1]) + fabs(drive[2]) + fabs(drive[3]);
+double sq(double x) {
+    return x * x;
 }
 
-//resulting herusutic function
-double herusutic(const double drive[4]) {
-	return powerConsumption(drive) + herusuticFunc(drive[0]) +
-			herusuticFunc(drive[1]) + herusuticFunc(drive[1]) + 
-			herusuticFunc(drive[3]);
+/* input: array of drivetrain voltage scalars. Output: rough power consumption */
+double powerConsumption(const double drive[4]) {
+	return sq(drive[0]) + sq(drive[1]) + sq(drive[2]) + sq(drive[3]);
+}
+
+//resulting heuristic function
+double heuristic(const double drive[4]) {
+	return powerConsumption(drive) + heuristicFunc(drive[0]) +
+			heuristicFunc(drive[1]) + heuristicFunc(drive[1]) + 
+			heuristicFunc(drive[3]);
 }
 
 /*input: relative sideways movement, relative back/forth movement, 
@@ -77,22 +81,56 @@ void driveOmniTrain(double xvel, double yvel, double t, double drivetrain[4]) {
 	double highbound = 4.0;
 	columnvec[3] = highbound;
 	multiplyMatrix(drivetrain_inverse_mat, columnvec, drivetrain);
-	double highval = herusutic(drivetrain);
+	double highval = heuristic(drivetrain);
 	
 	//set low bound
 	double lowbound = -4.0;
 	columnvec[3] = lowbound;
 	multiplyMatrix(drivetrain_inverse_mat, columnvec, drivetrain);
-	double lowval = herusutic(drivetrain);
+	double lowval = heuristic(drivetrain);
 	
 	double midval;
-	
-	for (int i = 0; i < 11; i++) {
-		if (highval < lowval) {
-			
-		}
+        int i;	
+	for (i = 0; i < 9; i++) {
+            if (highval < lowval) {
+                lowbound = (lowbound + highbound) * 0.5;
+            }
+            else if (highval > lowval) {
+                highbound = (lowbound + highbound) * 0.5;
+            }
+            columnvec[3] = highbound;
+	    multiplyMatrix(drivetrain_inverse_mat, columnvec, drivetrain);
+            highval = heuristic(drivetrain);
+
+            columnvec[3] = lowbound;
+            multiplyMatrix(drivetrain_inverse_mat, columnvec, drivetrain);
+            lowval = heuristic(drivetrain); 
 	}
-	
-	
+        columnvec[3] = (lowbound + highbound) * 0.5;
+        multiplyMatrix(drivetrain_inverse_mat, columnvec, drivetrain);
+        return;		
+}
+
+void boundedDriveOmniTrain(double xvel, double yvel, double t, double drivetrain[4]) {
+    driveOmniTrain(xvel, yvel, t, drivetrain);
+    int i;
+    double high = 0.0;
+    for (i = 0; i < 4; i++) {
+        if (fabs(drivetrain[i]) > high) {
+            high = fabs(drivetrain[i]);
+        }
+    }
+    if (fabs(high) > 1.00000000) {
+        #ifdef DEBUG
+        printf("%s", " \n The rent is too damn high! \n abs value of highest voltage scalar: ");
+        printf("%f", high);
+        #endif
+        double scalefactor = 1.000000 / fabs(high);
+        drivetrain[0] *= scalefactor;
+        drivetrain[1] *= scalefactor;
+        drivetrain[2] *= scalefactor;
+        drivetrain[3] *= scalefactor;
+
+    }
 }
 
