2769365183d66517c27fecc1da7993eeda639c96
TEAM3926ONE <agrabanski13@moundsparkacademy.org>
2013-02-12 17:04:58 -0600

    Finished code [v1]

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -24,9 +24,9 @@ public:
 		drivejags[1] = new CANJaguar(1, CANJaguar::kVoltage);
 		drivejags[2] = new CANJaguar(4, CANJaguar::kVoltage);
 		drivejags[3] = new CANJaguar(2, CANJaguar::kVoltage);
-		innerlift = new CANJaguar(6, CANJaguar::kVoltage);
-		outerlifts[0] = new CANJaguar(7, CANJaguar::kVoltage);
-		outerlifts[1] = new CANJaguar(5, CANJaguar::kVoltage);
+		innerlift = new CANJaguar(7, CANJaguar::kVoltage);
+		outerlifts[0] = new CANJaguar(5, CANJaguar::kVoltage);
+		outerlifts[1] = new CANJaguar(6, CANJaguar::kVoltage);
 	}
 
 	void Autonomous(void)
@@ -44,12 +44,36 @@ public:
 		}
 		setupOmniTrain(2.0, 2.0);
 		double drive[4];
+		double outerliftvoltage;
 		while (IsOperatorControl())
 		{
 			driveOmniTrain(stick1.GetX(), stick1.GetY(), stick2.GetX(), drive);
 			for (int i = 0; i < 4; i++) {
 				drivejags[i]->Set(drive[i] * 12.0);
 			}
+			if (stick1.GetRawButton(1)) {
+				innerlift->Set(11.0);
+			}
+			else if (stick1.GetRawButton(2)) {
+				innerlift->Set(-11.0);
+			}
+			else {
+				innerlift->Set(0.0);
+			}
+			
+			if (stick1.GetRawButton(3)) {
+				outerliftvoltage = 11.0;
+			}
+			else if (stick1.GetRawButton(4)) {
+				outerliftvoltage = -11.0;
+			}
+			else {
+				outerliftvoltage = 0.0;
+			}
+			outerlifts[0]->Set(outerliftvoltage);
+			outerlifts[1]->Set(outerliftvoltage);
+			
+			
 			//printf("%f", jag1.GetOutputVoltage());
 			//printf("\n");
 			Wait(1.0 / 30.0);				// wait for a motor update time
