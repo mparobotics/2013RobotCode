5801d56d1075fe050e4f92e2f0199cadd378b293
TEAM3926ONE <agrabanski13@moundsparkacademy.org>
2013-02-19 09:10:27 -0600

    Switched back to PWM, added "hang" button. Debug better

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -8,8 +8,9 @@ class RobotDemo : public IterativeRobot
 	RobotDrive myRobot; // robot drive system
 	Joystick stick1; // joystick1
 	Joystick stick2; //joystick2
-	CANJaguar* innerlifts[2];
-	CANJaguar* outerlifts[2];
+	Jaguar* innerlifts[2];
+	Jaguar* outerlifts[2];
+	DriverStation* ds;
 	int states[2];
 	
 	//Voltage variables
@@ -22,7 +23,7 @@ class RobotDemo : public IterativeRobot
 
 public:
 	RobotDemo(void):
-		myRobot(1, 2),	// these must be initialized in the same order
+		myRobot(8, 9),	// these must be initialized in the same order
 		stick1(1, 6, 12),		// as they are declared above.
 		stick2(2, 6, 12)
 	{
@@ -41,13 +42,14 @@ public:
 		controlChooser->AddObject("ATK3 Joysticks", &states[1]);
 		SmartDashboard::PutData("Control mode chooser", controlChooser);
 		
-		innerlifts[0] = new CANJaguar(2, CANJaguar::kPercentVbus);
-		innerlifts[1] = new CANJaguar(4, CANJaguar::kPercentVbus);
-		outerlifts[0] = new CANJaguar(1, CANJaguar::kPercentVbus);
-		outerlifts[1] = new CANJaguar(3, CANJaguar::kPercentVbus);
+		innerlifts[0] = new Jaguar(3);
+		innerlifts[1] = new Jaguar(4);
+		outerlifts[0] = new Jaguar(1);
+		outerlifts[1] = new Jaguar(2);
+		ds = DriverStation::GetInstance();
 		
 
-		
+		/*
 		//Initialize the lifts
 		for (int i = 0; i < 2; i++) {
 			outerlifts[i]->SetSpeedReference(CANJaguar::kSpeedRef_None);
@@ -57,7 +59,7 @@ public:
 		for (int i = 0; i < 2; i++) {
 			innerlifts[i]->SetSpeedReference(CANJaguar::kSpeedRef_None);
 			innerlifts[i]->EnableControl();
-		}
+		} */
 	}
 
 	virtual void TeleopInit() {
@@ -72,9 +74,12 @@ public:
 	
 	virtual void TeleopPeriodic()
 	{	
-		outerliftvoltage = 1.0 * joytrim(controlmethod ? stick1.GetRawAxis(2) : stick1.GetRawAxis(2));
+		outerliftvoltage = 0.9 * joytrim(controlmethod ? stick1.GetRawAxis(2) : stick1.GetRawAxis(2));
 		if (controlmethod ? stick1.GetRawButton(2) : stick1.GetRawButton(5)) {
-			outerliftvoltage = outerliftvoltage / 2.0;
+			outerliftvoltage = outerliftvoltage * 0.6;
+		}
+		if (stick1.GetRawButton(1)) {
+			outerliftvoltage += 0.3;
 		}
 		
 		outerlifts[0]->Set(outerliftvoltage);
@@ -84,28 +89,33 @@ public:
 		
 		innerliftvoltage = 1.0 * joytrim(controlmethod ? stick2.GetRawAxis(2) : stick1.GetRawAxis(5));
 		if (controlmethod ? stick2.GetRawButton(2) : stick1.GetRawButton(6)) {
-			innerliftvoltage = innerliftvoltage / 2.0;
+			innerliftvoltage = innerliftvoltage / 3.0;
 		}
 		innerlifts[0]->Set(innerliftvoltage);
 		innerlifts[1]->Set(innerliftvoltage);
 		
 #ifdef DEBUGTIME
-	/*	
+		float batvoltage = ds->GetBatteryVoltage();
 		printf("%s", "\n \n outer voltage: ");
 		printf("%f", outerliftvoltage);
+		printf("%s", "    ");
+		printf("%f", outerliftvoltage * batvoltage);
 		printf("%s", " \n inner voltage: ");
-		printf("%f", innerliftvoltage); */
+		printf("%f", innerliftvoltage);
+		printf("%s", "    ");
+		printf("%f", innerliftvoltage * batvoltage);
+		
+		/*
 		printf("%f", outerlifts[0]->Get());
 		printf("%s", "\n");
 		printf("%f", outerlifts[1]->Get());
 		printf("%s", "\n");
 		printf("%f", innerlifts[0]->Get());
-		printf("%f", innerlifts[1]->Get());
+		printf("%f", innerlifts[1]->Get()); */
 		printf("%s", "\n \n");
-		Wait(1.0);
 #endif
 		  
-		Wait((1.0 / 15.0));				// wait for a motor update time
+		Wait((1.0 / 30.0));				// wait for a motor update time
 	}
 };
 
