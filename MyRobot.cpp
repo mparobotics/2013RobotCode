5ce0c09359dd2ec068c5773ebd46e3c7a6d13345
TEAM3926ONE <agrabanski13@moundsparkacademy.org>
2013-03-15 11:34:22 -0500

    latest version
    BAM

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -1,8 +1,9 @@
 #include "WPILib.h"
 #include <Solenoid.h>
 #include "convenience.h"
-//#define DEBUGTIME
-
+#define DEBUGTIME
+//#define MOTORS_DISABLED
+ 
 class RobotDemo : public IterativeRobot
 {
 	RobotDrive myRobot; // robot drive system
@@ -11,6 +12,12 @@ class RobotDemo : public IterativeRobot
 	Jaguar* innerlifts[2];
 	Jaguar* outerlifts[2];
 	DriverStation* ds;
+	
+	DigitalInput* innerswitchb; //"b" for bottom
+	DigitalInput* innerswitcht;
+	DigitalInput* outerswitchb;
+	DigitalInput* outerswitcht;
+	
 	int states[2];
 	
 	//Voltage variables
@@ -38,6 +45,7 @@ public:
 	virtual void RobotInit() {
 		//Initialize the chooser in the dashboard between control modes
 		controlChooser = new SendableChooser();
+		controlmethod = false;
 		controlChooser->AddDefault("Xbox Controllers", &states[0]);
 		controlChooser->AddObject("ATK3 Joysticks", &states[1]);
 		SmartDashboard::PutData("Control mode chooser", controlChooser);
@@ -48,6 +56,13 @@ public:
 		outerlifts[1] = new Jaguar(2);
 		ds = DriverStation::GetInstance();
 		
+		outerswitchb = new DigitalInput(3); //front::bottom
+		outerswitcht = new DigitalInput(1); //front::top
+		innerswitchb = new DigitalInput(2); //inside::bottom
+		innerswitcht = new DigitalInput(4); //inside::top
+		//ALL WILL BE TRUE IF DEPRESSED
+		//IF DEPRESSED, STOP! ONLY ALLOW DRIVER TO MOVE IN THE RIGHT
+		//DIRECTION
 
 		/*
 		//Initialize the lifts
@@ -66,6 +81,7 @@ public:
 		
 		//False if Xbox controllers, true if joysticks
 		controlmethod = (bool) *((int *)controlChooser->GetSelected());
+		controlmethod = false;
 		
 		
 		myRobot.SetSafetyEnabled(false);
@@ -75,24 +91,46 @@ public:
 	virtual void TeleopPeriodic()
 	{	
 		outerliftvoltage = 0.9 * joytrim(controlmethod ? stick1.GetRawAxis(2) : stick1.GetRawAxis(2));
-		if (controlmethod ? stick1.GetRawButton(2) : stick1.GetRawButton(5)) {
+		if (controlmethod ? stick1.GetRawButton(2) : stick1.GetRawButton(9)) {
 			outerliftvoltage = outerliftvoltage * 0.6;
 		}
-		if (stick1.GetRawButton(1)) {
+		if (stick1.GetRawButton(6) or stick1.GetRawButton(5)) {
 			outerliftvoltage += 0.3;
 		}
+		//HOOK DOWN IS POSITIVE (robot up)\
+		//If bottom is triggered, just make sure it doesn't move up
+		/*
+		if (outerswitchb->Get()) {
+			outerliftvoltage = outerliftvoltage < 0.0 ? outerliftvoltage : 0.0;
+		} */
+		//If top is triggered, we need to hang
 		
+		if (!outerswitcht->Get()) {
+			outerliftvoltage = outerliftvoltage > 0.0 ? outerliftvoltage : 0.0;
+		} 
+		
+#ifndef MOTORS_DISABLED
 		outerlifts[0]->Set(outerliftvoltage);
 		outerlifts[1]->Set(-outerliftvoltage);
-		
+#endif		
 
-		
+		//UP is negative, down is positive [joystick]
 		innerliftvoltage = 1.0 * joytrim(controlmethod ? stick2.GetRawAxis(2) : stick1.GetRawAxis(5));
-		if (controlmethod ? stick2.GetRawButton(2) : stick1.GetRawButton(6)) {
+		if (controlmethod ? stick2.GetRawButton(2) : stick1.GetRawButton(10)) {
 			innerliftvoltage = innerliftvoltage / 3.0;
 		}
+		
+		if (!innerswitchb->Get()) {
+			innerliftvoltage = innerliftvoltage < 0 ? innerliftvoltage : 0.0;
+		} /*
+		if (innerswitcht->Get()) {
+			innerliftvoltage = innerliftvoltage > 0 ? innerliftvoltage : 0.0;
+		} */
+	
+#ifndef MOTORS_DISABLED
 		innerlifts[0]->Set(innerliftvoltage);
 		innerlifts[1]->Set(innerliftvoltage);
+#endif
 		
 #ifdef DEBUGTIME
 		float batvoltage = ds->GetBatteryVoltage();
@@ -113,6 +151,7 @@ public:
 		printf("%f", innerlifts[0]->Get());
 		printf("%f", innerlifts[1]->Get()); */
 		printf("%s", "\n \n");
+		Wait((1.0 / 12.0));
 #endif
 		  
 		Wait((1.0 / 30.0));				// wait for a motor update time
